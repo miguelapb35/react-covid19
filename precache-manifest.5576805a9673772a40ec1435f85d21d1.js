a0e1fc37543f88b5e1a0c31084a4eb0a7b23dc2c
Miguel Angel <miguelapb35@gmail.com>
2020-04-15 15:00:53 -0500

    Updates

---
+++ precache-manifest.5576805a9673772a40ec1435f85d21d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbf6361130974b75b6fd8b728aa6c8b7",
+    "url": "/react-covid19/index.html"
+  },
+  {
+    "revision": "051d4e386dbd6776b378",
+    "url": "/react-covid19/static/css/main.9627c54a.chunk.css"
+  },
+  {
+    "revision": "8fc3c771e6f27e6db771",
+    "url": "/react-covid19/static/js/2.b6660088.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/react-covid19/static/js/2.b6660088.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "051d4e386dbd6776b378",
+    "url": "/react-covid19/static/js/main.4fdaae0d.chunk.js"
+  },
+  {
+    "revision": "88103659f8915eb3015b",
+    "url": "/react-covid19/static/js/runtime-main.592ca940.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/react-covid19/static/media/image.d7265326.png"
+  }
+]);
